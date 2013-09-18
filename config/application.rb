5bd705d753a21d9a3f8fea17070e8b5085a78091
Juan Tabares <juan.ctt@live.com>
2013-09-19 01:55:28 +0900

    avoid compilation of assets

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,9 @@ module Kanjime
     config.assets.initialize_on_precompile = false
     
     # Enable the asset pipeline
-    config.assets.enabled = true
-    
+    config.assets.enabled = true    
     config.assets.compile = true
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .gif )
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
