278f8a21764f906216bd9ce61bb7cbd874437e70
Juan Tabares <juan.ctt@live.com>
2013-09-19 02:52:02 +0900

    fixing compilation of assets

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module Kanjime
     config.assets.enabled = true
     
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
