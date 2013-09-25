96c3e1fea08cc5944a3e38b95963d9048bdf5467
Juan Tabares <juan.ctt@live.com>
2013-09-25 16:17:00 +0900

    changing views in users, added SSL support for production environment

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Kanjime::Application.routes.draw do
   
   resources :users
   root :to => 'pages#home'
+  match '/signup',  to: 'users#new', via: 'get'
   
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do
+  factory :user do
+    name     "Juan Tabares"
+    email    "juan.tabares@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
