4bd45d43466573788795a93233fd34dfd7a6ffa4
Juan Tabares <juan.ctt@live.com>
2013-09-27 11:14:14 +0900

    sort users by created in ascending order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page], :per_page => 10)
+    @users = User.paginate(page: params[:page], :per_page => 10).order('created_at ASC')
   end
 
   # GET /users/1
