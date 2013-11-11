67d968c18a33a91f48305697e471ba08d8b71d09
Juan Tabares <juan.ctt@live.com>
2013-11-11 16:50:44 +0900

    A working follow button the standard way
    - Added the Relationship controller
    - Added validations for the relationship controller
    - Added validations to prevent non-signed in users to follow/unfollow

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -71,6 +71,18 @@ describe "Authentication" do
         end
       end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end      
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
