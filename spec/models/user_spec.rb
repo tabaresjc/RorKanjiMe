d209fd0cdae7ebea00c85ff977e30ca9ed49797f
Juan Tabares <juan.ctt@live.com>
2013-11-25 18:25:21 +0900

    New status feed
    - Pull all micropost from users being followed by current user and by the current user too

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -164,15 +164,26 @@ describe User do
       end
     end
 
-		describe "status" do
+    describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum", title: "Lorem ipsum title") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
-    end    
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end      
 	end
 
   describe "following" do
